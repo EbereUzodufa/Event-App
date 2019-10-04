15afae5e598c0d47830ed1d63bfa5eaedbd8ce28
Ebere <ebereuzodufa@gmail.com>
2019-10-04 16:08:09 +0100

    Adds image to template

--- src/app/events-app.component.ts
+++ src/app/events-app.component.ts
@@ -2,7 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'event-app',
-  template: '<h2>Hello World</h2>',
+  template: `<h2>Hello World</h2>
+            <img src="/assets/images/basic-shield.png" />
+  `,
 })
 export class EventsAppComponent {
   title = 'App';
