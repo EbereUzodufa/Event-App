c32993f4656e12158973cf3ed2c43fc1aec70d3f
Ebere <ebereuzodufa@gmail.com>
2019-10-05 14:50:21 +0100

    Creates HTML componrnt for Event-list & moves HTML

--- src/app/events/event-list.component.ts
+++ src/app/events/event-list.component.ts
@@ -2,14 +2,7 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'events-list',
-    template: `
-        <div>
-            <h1>Upcoming Angular Events</h1>
-            <hr>
-
-            <h2>{{event.name}}</h2>
-        </div>
-    `
+    templateUrl: './event-list.component.html'
 })
 
 export class EventListComponent{
