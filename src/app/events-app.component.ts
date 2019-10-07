511d5478ca398281287bc54f02245cd898663475
Ebere <ebereuzodufa@gmail.com>
2019-10-07 14:26:01 +0100

    MAde my work neater

--- src/app/events-app.component.ts
+++ src/app/events-app.component.ts
@@ -2,7 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'event-app',
-  template: '<events-list></events-list>',
+  template: `
+      <navBar></navBar>
+      <events-list></events-list>
+    `,
 })
 export class EventsAppComponent {
   title = 'App';
--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -5,6 +5,7 @@ import { Component, Input, Output, EventEmitter } from "@angular/core";
     templateUrl: './event-thumbnail.component.html',
     styles:[
         `
+        .thumbnail{min-height: 210px;}
         .pad-left{margin-left: 15px}
         `
     ]
