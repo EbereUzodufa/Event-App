ef1e8ea50d17c35c46e0693778b3f407f119f95a
Ebere <ebereuzodufa@gmail.com>
2019-10-08 13:27:54 +0100

    Use [ngClass] to set class based on time

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -5,7 +5,9 @@ import { Component, Input, Output, EventEmitter } from "@angular/core";
     templateUrl: './event-thumbnail.component.html',
     styles:[
         `
-        .even{ color: pink}
+        .green { color: green !important}
+        .bold { font-weight: bold}
+        .even, .pink{ color: pink}
         .thumbnail{min-height: 210px;}
         .pad-left{margin-left: 15px}
         `
@@ -24,4 +26,15 @@ export class EventThumbnailComponent{
     // handleClickMe(){
     //     this.eventClick.emit(this.event.name);
     // }
+
+    getStartTimeClass(){
+        //This function get's the approprate class based on Start time class
+        if(this.event && this.event.time === '8:00 am'){
+            return ['green', 'bold'];
+        } else if(this.event && this.event.time === '9:00 am'){
+            return ['pink', 'bold'];
+        }
+            
+        return [];
+    }
 }
