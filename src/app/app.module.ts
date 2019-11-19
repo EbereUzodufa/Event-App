fe32358d92626bfe9f1bdf4dac7d011d2303cc5c
Ebere <ebereuzodufa@gmail.com>
2019-11-19 09:10:02 +0100

    Adds service for active and deactive routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,8 +9,11 @@ import { NavBarComponent } from './nav/navbar.component';
 import { EventService } from './events/shared/events.services';
 import { ToastrService } from './common/toastr.service';
 import { EventDetailsComponent } from './events/event-details/event-details.component';
+import { CreateEventComponent } from './events/create-event.component';
+import { Error404Component } from './errors/404.component';
 import { appRoute } from './routes';
 import { CommonModule } from '@angular/common';
+import { EventRouteActivator } from './events/event-details/event-route-activator.service';
 
 @NgModule({
   imports: [
@@ -23,12 +26,27 @@ import { CommonModule } from '@angular/common';
     EventListComponent,
     EventThumbnailComponent,
     NavBarComponent,
-    EventDetailsComponent
+    EventDetailsComponent,
+    CreateEventComponent,
+    Error404Component
   ],
   providers: [
     EventService,
-    ToastrService
+    ToastrService,
+    EventRouteActivator,
+    {
+      provide: 'canDeactivateCreateEvent', 
+      useValue: checkDirtyState
+    }
   ],
   bootstrap: [EventsAppComponent]
 })
 export class AppModule { }
+
+export function checkDirtyState(component:CreateEventComponent) {
+  if (component.isDirty) {
+    return window.confirm('You have not saved this event, do you really want to cancel?');
+  } else{
+    true;
+  }
+}
