6c98f59b35db638dcae9dbdecead0b19029111aa
Ebere <ebereuzodufa@gmail.com>
2020-01-14 21:34:43 +0100

    Uses inbuild angular pipe

--- src/app/common/collapsible-well.component.ts
+++ src/app/common/collapsible-well.component.ts
@@ -4,14 +4,16 @@ import { Component, Input } from "@angular/core";
     selector: 'component-well',
     template: `
     <div (click)="toggleContent()" class="well pointable">
-        <h4 class="well-title">{{title}}</h4>
-        <ng-content *ngIf="visibleSection"></ng-content>
+        <h4>
+            <ng-content select="[well-title]"></ng-content>
+        </h4>
+        <ng-content *ngIf="visibleSection" select="[well-body]"></ng-content>
     </div>
     `
 })
 
 export class CollapsibleWellComponent{
-    @Input() title: string;
+    // @Input() title: string;
     visibleSection: boolean = true;
 
     toggleContent(){
