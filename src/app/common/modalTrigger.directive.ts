67e2c9febb5a19dff06b64d94cc560a88bcccfcf
Ebere <ebereuzodufa@gmail.com>
2020-03-19 16:11:04 +0100

    Used jquery injection to call modal close

--- src/app/common/modalTrigger.directive.ts
+++ src/app/common/modalTrigger.directive.ts
@@ -1,6 +1,7 @@
-import { Directive, OnInit, ElementRef, Input } from "@angular/core";
+import { Directive, OnInit, ElementRef, Input, Inject } from "@angular/core";
+import { JQ_TOKEN } from './jquery.service';
 
-declare var $: any;
+// declare var $: any;
 
 @Directive({
     selector: '[modal-trigger]'
@@ -10,14 +11,14 @@ export class ModalTriggerDirective implements OnInit{
     private el: HTMLElement;
     @Input('modal-trigger') modalId:string;
 
-    constructor(ref:ElementRef){
+    constructor(ref:ElementRef, @Inject(JQ_TOKEN) private $ : any){
         this.el = ref.nativeElement;
     }
 
     ngOnInit(){
         this.el.addEventListener('click', e=>{
             // alert('sd');
-            $(`#${this.modalId}`).modal('show');
+            this.$(`#${this.modalId}`).modal('show');
         })
     }
 }
